f9a2b661c6d36c893b41f6efcf6ed16e877cb5ff
thiane gueye <thiane.gueye@ism.edu.sn>
2025-12-17 20:58:34 +0000

    fix: syntax error in Program.cs (missing semicolon)

--- csharp/brasil_burger/Program.cs
+++ csharp/brasil_burger/Program.cs
@@ -22,7 +22,7 @@ app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Home}/{action=Index}/{id?}");
     
 
 
