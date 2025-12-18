108dff5fecd86b906516bcac6f077032b9533800
thiane gueye <thiane.gueye@ism.edu.sn>
2025-12-18 23:46:44 +0000

    Correction ⇒ Mappage complet des colonnes minuscules pour PostgreSQL

--- csharp/brasil_burger/Models/Complement.cs
+++ csharp/brasil_burger/Models/Complement.cs
@@ -4,14 +4,18 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace brasil_burger.Models;
 
 [Table("complements")]
-public class Complement {
+public class Complement
+{
     [Key, Column("id_complement")]
     public int Id { get; set; }
+    [Column("nom")]
     public string Nom { get; set; } = null!;
+    [Column("prix")]
     public decimal Prix { get; set; }
+    [Column("image")]
     public string? Image { get; set; }
     [Column("type_complement")]
-    public string TypeComplement { get; set; } = null!; // Boisson, Frites, Sauce
+    public string TypeComplement { get; set; } = null!;
     [Column("est_archive")]
     public bool EstArchive { get; set; }
 }
--- csharp/brasil_burger/Models/Menu.cs
+++ csharp/brasil_burger/Models/Menu.cs
@@ -4,11 +4,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace brasil_burger.Models;
 
 [Table("menus")]
-public class Menu {
+public class Menu
+{
     [Key, Column("id_menu")]
     public int Id { get; set; }
+    [Column("nom")]
     public string Nom { get; set; } = null!;
+    [Column("prix")]
     public decimal Prix { get; set; }
+    [Column("image")]
     public string? Image { get; set; }
     [Column("est_archive")]
     public bool EstArchive { get; set; }
