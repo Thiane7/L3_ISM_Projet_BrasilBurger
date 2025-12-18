b8fe34c02ecea11913ba0904c8e219ff6bc75d09
thiane gueye <thiane.gueye@ism.edu.sn>
2025-12-18 23:32:34 +0000

    Correction ⇒ Mappage explicite des colonnes en minuscules pour PostgreSQL

--- csharp/brasil_burger/Models/Burger.cs
+++ csharp/brasil_burger/Models/Burger.cs
@@ -4,11 +4,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace brasil_burger.Models;
 
 [Table("burgers")]
-public class Burger {
+public class Burger
+{
     [Key, Column("id_burger")]
     public int Id { get; set; }
+    [Column("nom")]
     public string Nom { get; set; } = null!;
+    [Column("prix")]
     public decimal Prix { get; set; }
+    [Column("image")]
     public string? Image { get; set; }
     [Column("est_archive")]
     public bool EstArchive { get; set; }
