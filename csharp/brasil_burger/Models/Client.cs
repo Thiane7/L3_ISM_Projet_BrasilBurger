10b69686baa1000fe7c6640ef087479e7d451953
thiane gueye <thiane.gueye@ism.edu.sn>
2025-12-20 01:41:14 +0000

    Modèle: Ajout de la classe Client pour le mapping Neon SQL

---
+++ csharp/brasil_burger/Models/Client.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace brasil_burger.Models
+{
+    [Table("clients")]
+    public class Client
+    {
+        [Key]
+        [Column("id_client")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("nom")]
+        public string Nom { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [Column("email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [Column("mot_de_passe")]
+        public string MotDePasse { get; set; } = string.Empty;
+
+        [Column("telephone")]
+        public string? Telephone { get; set; }
+    }
+}
